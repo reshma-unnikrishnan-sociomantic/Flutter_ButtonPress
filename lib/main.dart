c20a7442ccbadabed670d510aefea90525c9c2b8
reshma.unnikrishnan@sociomantic.com <reshma.unnikrishnan@sociomantic.com>
2018-08-02 14:13:46 +0200

    Added raisedButton on the Scafold

--- lib/main.dart
+++ lib/main.dart
@@ -1,27 +1,49 @@
 
 
-import 'package:flutter/material.dart'
+import 'package:flutter/material.dart';
 
 main() {
+  runApp(MyApp());
 
 }
 
-class CounterState extends State<Counter> {
-  int counter = 0;
+class MyApp extends StatelessWidget {
+@override 
+Widget build(BuildContext context) {
+  return new MaterialApp(title: 'Generated App',
+  theme: new ThemeData(primaryColor: Colors.grey),
+  home: new MyHomePage(),
+  );
+}
+}
 
-  void increment() {
-    setState(() {
-    counter++;
-   });
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
 
-  }
+class _MyHomePageState extends State<MyHomePage> {
  
-  Widget build(BuildContext context) {
-    return Row(children: <Widget>[RaisedButton(onPressed: increment,
-    child: Text('increment'),
-    ),
-    Text('Counter: $counter'),
-    ],
-    );
-  }
+    @override
+      Widget build(BuildContext context) {
+        // TODO: implement build
+        return new Scaffold(appBar: new AppBar(
+          title: new Text('Welcome to Flutter'),
+        ),
+        body: new RaisedButton(key: null,
+        onPressed: buttonPressed,
+        color: Colors.green,
+        child: new Text("Button 1", style: new TextStyle(fontSize: 13.0, color: const Color(0xFF000000))
+        ),
+        ),    
+        );
+    
+
+      }
+
+      void buttonPressed(){
+
+
+      }
 }
+
